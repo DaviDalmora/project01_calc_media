16dc42a7dac55550991a65082c3125a4d4156f84
DaviDalmora <129559771+DaviDalmora@users.noreply.github.com>
2023-07-03 22:02:45 -0300

    Delete project01_calc_media_ directory

--- project01_calc_media_/teste/testejs.js
+++
@@ -1,9 +0,0 @@
-const adicionar = document.getElementById("adicionar");
-const ingredientes = document.getElementById("ingredientes");
-
-adicionar.addEventListener("click", function (event) {
-  let campo = document.createElement("input");
-  campo.name = "ingredientes";
-  campo.placeholder = "Ex: Sal";
-  ingredientes.appendChild(campo);
-});
